d33677e078f7eb7b13de2b43a21ccea8d9db87f3
ishioka <ishioka@iij.ad.jp>
2014-06-09 07:30:57 +0900

    v0.0.1

--- Rakefile
+++ Rakefile
@@ -13,3 +13,14 @@ task :console do
     sh 'irb -rubygems -I lib'
 end
 task :c => :console
+
+=begin
+require 'rake/testtask'
+Rake::TestTask.new(:test) do |test|
+  test.libs << 'lib' << 'test'
+  test.pattern = 'test/**/test_*.rb'
+  test.verbose = true
+end
+
+task :default => :test
+=end
--- spec/in_mytail_spec.rb
+++ spec/in_mytail_spec.rb
@@ -1,17 +1,18 @@
 require 'spec_helper'
 
+require 'tempfile'
+
 
 describe Fluent::MyTailInput do
   before { Fluent::Test.setup }
-  TMP_DIR = File.dirname(__FILE__) + "/tmp"
   CONFIG = %[
     path ${path}
-    format /^(?<time>.*) (?<req_time>.*)/
+    format /^(?<time>\\d*\\.\\d*) (?<req_time>\\d*)$/
     time_format %s.%L
     to_i ${tois}
     tag ${tag}
   ]
-  let(:path)   { "#{TMP_DIR}/squid2.log" }
+  let(:path)   { "/var/service/cas/ats/logs/squid2.log" }
   let(:driver) { Fluent::Test::InputTestDriver.new(Fluent::MyTailInput).configure(config)}
   let(:tag)    { 'ats.cache' }
   let(:tois)   { 'time' }
@@ -41,40 +42,36 @@ describe Fluent::MyTailInput do
   end
 
   describe 'test emits' do
-    let(:time) { Time.now }
-    #let(:emits) do
-    #  driver.run do
-    #    File.open(path,'a'){|f|
-    #      f.puts "12345.12345 99999"
-    #    }
-    #  end
-    #end
-    
-    context 'typical usage' do
-      let(:config) { CONFIG }
-      #before do
-        #Fluent::Engine.should_receive(:emits).with(tag, time.to_i, {'time'=>'123345', 'req_time' => '99999'} )
-      #end
-      it { 
-          d = Fluent::Test::InputTestDriver.new(Fluent::MyTailInput).configure(
-   %[
-    path  /root/work/fluent/fluent-plugin-mytail/spec/tmp/squid2.log 
-    format /^(?<time>.*) (?<req_time>.*)/
-    time_format %s.%L
-    to_i time
-    tag ats.cache
-  ]
-
-)
-          d.run do
-            File.open("/root/work/fluent/fluent-plugin-mytail/spec/tmp/squid2.log",'w'){|f|
-            f.puts '1399642335.109 123'
-          }
-          end
-          e = d.emits
-          p e.length
-       } 
-    end 
+    let(:tmpf) {Tempfile.new("in_tail_multiline2-") }
+    let(:config) { 
+    %[
+       path #{tmpf.path}
+       format /^(?<time>\\d*\\.\\d*) (?<req_time>\\d*)$/
+       time_format %s.%L 
+       to_i time
+       tag ats.cache
+     ]
+    }
+    let(:emits) do
+       begin
+         d = Fluent::Test::InputTestDriver.new(Fluent::MyTailInput).configure config
+         d.run do
+           File.open(tmpf,'w'){|f|
+             f.puts "1392259627.123 1230"
+             f.puts "1492259627.123 1230"
+           }
+           sleep 1
+         end
+         d.emits
+       ensure
+         tmpf.close
+       end
+    end
+    #context 'typical usage' do
+      it { expect( emits.length).to eq 2 }
+      it { expect( emits[0] ).to match_array ["ats.cache",1392259627,{'req_time' => "1230",'time' => 0}] }
+      it { expect( emits[1] ).to match_array ["ats.cache",1492259627,{'req_time' => "1230",'time' => 0}] }
+    #end 
   end
   
 
